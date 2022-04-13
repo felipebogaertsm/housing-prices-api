85b8c0d5988230c7eb4313d53b32670fb5bf228f
Felipe Bogaerts de Mattos <felipebogaerts@gmail.com>
2022-04-13 09:43:27 -0300

    0.1.1
    Improved VivaReal scraper

--- api/scrapers/viva_real.py
+++ api/scrapers/viva_real.py
@@ -17,12 +17,24 @@ class VivaReal(SourceWebsite):
         self.currency = "BRL"
 
     def get_housing_units(self):
-        response = requests.get(BASE_URL)
-        doc = BeautifulSoup(response.text, "html.parser")
+        results_list = []
 
-        results_list = doc.find("div", {"class": "results-list"}).findChildren(
-            "div", recursive=False
-        )
+        i = 0
+        response = requests.get(BASE_URL)
+        while int(response.status_code / 100) == 2:  # status code in 200 range
+            response = requests.get(BASE_URL, params={"pagina": i + 1})
+            doc = BeautifulSoup(response.text, "html.parser")
+
+            try:
+                results_list.append(
+                    doc.find("div", {"class": "results-list"}).findChildren(
+                        "div", recursive=False
+                    )
+                )
+            except AttributeError:  # if page not found error
+                break
+
+            i += 1
 
         self.housing_units = []
 
@@ -30,14 +42,20 @@ class VivaReal(SourceWebsite):
             price = item.find("div", {"class": "property-card__price"}).find("p").text
             price_value = float(only_numbers(price, decimal_separator=","))
 
+            address = item.find("span", {"class": "property-card__address"}).text
+            address_split = address.split(" - ")
+            address_street = address_split[0]
+            address_area = address_split[1].split(", ")[0]
+            address_city = address_split[1].split(", ")[1]
+
             self.housing_units.append(
                 HousingUnit(
                     address=Address(
-                        area="",
-                        city="",
-                        country="",
+                        area=address_area,
+                        city=address_city,
+                        country=self.country,
                         postal_code="",
-                        street="",
+                        street=address_street,
                     ),
                     price=Price(value=price_value, currency=self.currency),
                 )
